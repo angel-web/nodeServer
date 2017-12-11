2f22387250b39aea7d554f36c607768826dbfa36
疯子 <17737466337@example.com>
2017-12-11 23:03:01 +0800

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('build'))
+
+app.get('*', function (req, res, next) {
+  var options = {
+    root: __dirname + '/build/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = 'index.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      // console.log('Sent:', fileName);
+    }
+  });
+})
+
+var server = app.listen(80, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
